d8dc55b2655f8eb7adca557199d5373bdce4aa38
huiseung <whtjddn75@gmail.com>
2023-04-24 00:34:05 +0900

    init multi module

---
+++ Backend/api/src/main/java/com/example/ApiApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@ComponentScan({"com.example"})
+@EnableJpaRepositories({"com.example.core.repository"})
+@EntityScan({"com.example.core.entity"})
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
