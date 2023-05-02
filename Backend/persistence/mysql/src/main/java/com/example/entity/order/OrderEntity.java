afe87580dc3438c954945182c6940b3dda593148
huiseung <whtjddn75@gmail.com>
2023-05-02 22:49:19 +0900

    <feat> : order, orderEntity add ^M ^M

---
+++ Backend/persistence/mysql/src/main/java/com/example/entity/order/OrderEntity.java
@@ -0,0 +1,57 @@
+package com.example.entity.order;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Table(name="orders")
+@Getter
+@Entity
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class OrderEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="order_id")
+    private Long orderId;
+
+    @Column(name="user_id")
+    private Long userId;
+    @Column(name="product_id")
+    private Long productId;
+    @Column(name="product_option_id")
+    private Long productOptionId;
+
+    @Column(name="product_name")
+    private String productName;
+    @Column(name="shop_name")
+    private String shopName;
+    @Column(name="price")
+    private Integer price;
+    @Column(name="quantity")
+    private Integer quantity;
+    @Column(name="product_option_name")
+    private String productOptionName;
+
+    @Column(name="created_dt")
+    private Instant createdDt;
+    @Column(name="updated_dt")
+    private Instant updatedDt;
+
+    @PrePersist
+    public void prePersist(){
+        this.createdDt = Instant.now();
+        this.updatedDt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        this.updatedDt = Instant.now();
+    }
+}
--- Backend/persistence/mysql/src/main/java/com/example/entity/product/ProductMapper.java
+++ Backend/persistence/mysql/src/main/java/com/example/entity/product/ProductMapper.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 @Component
 @Getter
 @NoArgsConstructor
-
 public class ProductMapper {
     public Product toDomain(ProductEntity productEntity){
         return Product.builder()
