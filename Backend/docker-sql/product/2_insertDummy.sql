3c82afb7d1b5b61a0a49099f2da81b1d8f95f693
홍석현 <seokh1213@gmail.com>
2023-05-26 23:48:12 +0900

    feat: sql 변경

--- Backend/docker-sql/product/2_insertDummy.sql
+++ Backend/docker-sql/product/2_insertDummy.sql
@@ -4,9 +4,6 @@ values ('빈폴');
 insert into categories(parent_category_id, has_child_category, content)
 values (null, false, '바지');
 
-insert into photos(user_id, encoded_name, original_name, path, file_extension)
-values (1, 'UNIQUE_ENCODED_HASH', '1_1_1.jpg', 'post/UNIQUE_ENCODED_HASH', 'jpg');
-
 insert into products(shop_id, thumbnail_photo_id, category_id, sub_category_id, sale_id, serial_number, product_state,
                      product_name, product_desc, gender, price, register_dt)
 values (1, 1, 1, null, null, 'SERIAL_NUMBER', 'REGISTERED', '데님 바지', '남자 데님 바지', 'MALE', 78000, null);
